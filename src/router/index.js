9f7b9cd0b038240f8ef46867d96e5f7dce674c8e
gyxj <51393387+gyxj@users.noreply.github.com>
2019-06-06 17:39:09 +0800

    vue高仿饿了么

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import goods from '../components/goods/goods.vue'
+import seller from '../components/seller/seller.vue'
+import ratings from '../components/ratings/ratings.vue'
+import '../common/stylus/index.styl'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/goods',
+      name: 'goods',
+      component: goods
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: seller
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: ratings
+    },
+    {
+      path: '/', redirect: '/goods'
+    }
+  ]
+})
